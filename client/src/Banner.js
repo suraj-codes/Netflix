d1060a2923912783980e2b4c7a687697170bb51f
Suraj Deshmane <surajdeshmane05@gmail.com>
2021-04-25 21:19:53 +0530

    Removing firebase and using MongoDB

--- client/src/Banner.js
+++ client/src/Banner.js
@@ -17,9 +17,7 @@ const Banner = () => {
       return request;
     }
     fetchData();
-    if(movie?.title=="undefined" || movie?.name=="undefined" || movie?.original_name=="undefined"){
-      fetchData();
-    }
+    
   }, [movie]);
   const truncate = (string, n) => {
     return string.length > n ? string.substring(0, n - 1) + " ..." : string;
